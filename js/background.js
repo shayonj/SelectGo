084eecc95a3423e6de3001e6dddcc9b49a154eb0
Shayon Mukherjee <dev@shayon.me>
2015-03-06 15:17:48 -0600

    Added select options and introduced storage api

--- js/background.js
+++ js/background.js
@@ -18,3 +18,16 @@ chrome.runtime.onMessage.addListener(function(request) {
   }
 
 });
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (key in changes) {
+    var storageChange = changes[key];
+    console.log('Storage key "%s" in namespace "%s" changed. ' +
+    'Old value was "%s", new value is "%s".',
+    key,
+    namespace,
+    storageChange.oldValue,
+    storageChange.newValue);
+  }
+});
+
--- js/select.js
+++ js/select.js
@@ -19,16 +19,31 @@ SelectGo.Selector.mouseup = function(){
   var text = SelectGo.Selector.getSelected();
   if(text!=''){
     chrome.runtime.sendMessage({
-        type: 'tab',
-        text: text
+      type: 'tab',
+      text: text
     });
     chrome.runtime.sendMessage({
-        type: 'copy',
-        text: text
-    }); 
+      type: 'copy',
+      text: text
+    });
   }
 }
 
 $(document).ready(function(){
   $(document).on("mouseup", SelectGo.Selector.mouseup);
+  // Trigger action on option select
+  $("#save").click(function() {
+    saveChanges();
+  });
+
 });
+
+function saveChanges() {
+  var theValue = $("input[name='selectVal']:checked").val();
+  alert(theValue); // Debug
+
+  // Save option selected and sync
+  chrome.storage.sync.set({'option': theValue}, function() {
+    console.log(theValue);
+  });
+}
